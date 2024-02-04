9533219feac3633224d1843c40ca61f6c114519c
Minh Hứa <minh.huaanh@employmenthero.com>
2024-02-05 00:27:48 +0700

    Add main route

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { DevSupport } from '@react-buddy/ide-toolbox'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import './scss/styles.scss'
 
 import App from './App.tsx'
 import { ComponentPreviews, useInitial } from '~/dev'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
-      <App />
+      <RouterProvider router={router} />
     </DevSupport>
   </React.StrictMode>
 )
