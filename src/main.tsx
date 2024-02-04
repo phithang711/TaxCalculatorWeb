c32133f894beb303382bf45f4091a77ae6a64f09
Minh Hứa <minh.huaanh@employmenthero.com>
2024-02-05 00:48:20 +0700

    Config store

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { DevSupport } from '@react-buddy/ide-toolbox'
+import { Provider } from 'react-redux'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import './scss/styles.scss'
+import '~/styles/index.scss'
 
 import App from './App.tsx'
 import { ComponentPreviews, useInitial } from '~/dev'
+import { store } from '~/configs/store.config.ts'
 
 const router = createBrowserRouter([
   {
@@ -18,7 +20,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </DevSupport>
   </React.StrictMode>
 )
