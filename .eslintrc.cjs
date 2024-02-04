9107d87d8adcbf71b03247df4d7a61a573e84023
Minh Hứa <minh.huaanh@employmenthero.com>
2024-02-04 09:11:39 +0700

    Add hook pre commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts', 'dev'],
   parser: '@typescript-eslint/parser',
