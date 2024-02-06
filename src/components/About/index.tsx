ef7237b833e68391ef3218c737920dbbba3e8117
Minh Hứa <minh.huaanh@employmenthero.com>
2024-02-06 16:05:37 +0700

    Remove temp log

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,14 +1,4 @@
-import { useEffect } from 'react'
-import ErrorType from '~/types/error'
-
 const About = () => {
-  useEffect(() => {
-    throw {
-      error: new Error('About error'),
-      data: 'About error'
-    } as unknown as ErrorType
-  }, [])
-
   return <div>About</div>
 }
 
