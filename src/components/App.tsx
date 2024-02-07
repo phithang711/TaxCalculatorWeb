f817993f1a0e76f3aaf00619537c40639af93ffc
Minh Hứa <minh.huaanh@employmenthero.com>
2024-02-08 00:37:22 +0700

    Add i18N

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,7 @@ import About from '~/components/About'
 import Home from '~/components/Home'
 import Page404 from '~/components/Page404'
 import ErrorBoundary from '~/components/ErrorBoundary'
+import { Suspense } from 'react'
 
 const router = sentryCreateBrowserRouter([
   {
@@ -24,7 +25,11 @@ const router = sentryCreateBrowserRouter([
 ])
 
 const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <Suspense>
+      <RouterProvider router={router} />
+    </Suspense>
+  )
 }
 
 export default App
