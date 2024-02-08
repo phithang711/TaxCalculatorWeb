c6bca2cdc17c3730f5c14d899f3f358250e4712a
Minh Hứa <minh.huaanh@employmenthero.com>
2024-02-08 10:25:58 +0700

    Testing error trigger for sentry

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,5 +1,23 @@
+import ErrorType from '~/types/error'
+
 const About = () => {
-  return <div>About</div>
+  const triggerError = () => {
+    throw {
+      data: 'Error',
+      error: new Error('Error')
+    } as ErrorType
+  }
+
+  return (
+    <div>
+      <h1>About</h1>
+      {import.meta.env.MODE !== 'production' && (
+        <button className='btn btn-primary' onClick={triggerError}>
+          Trigger an error
+        </button>
+      )}
+    </div>
+  )
 }
 
 export default About
