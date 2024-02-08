d1ce80c6247497f42b60ac82dd53be0cdd331b2a
Minh Hứa <minh.huaanh@employmenthero.com>
2024-02-08 11:08:21 +0700

    Log env

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 console.log('SENTRY_AUTH_TOKEN: ', process.env.SENTRY_AUTH_TOKEN)
+console.log(process.env.NODE_ENV)
 
 // https://vitejs.dev/config/
 export default defineConfig({
