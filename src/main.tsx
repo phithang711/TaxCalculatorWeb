0cd990f1b7f6e7a575af40397e4338878def233c
Minh Hứa <minh.huaanh@employmenthero.com>
2024-02-08 09:17:20 +0700

    Add env for staging

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,9 @@ import App from './components/App.tsx'
 import { store } from '~/configs/store.config.ts'
 import '~/configs/i18n.config.ts'
 
+console.log(process.env.NODE_ENV)
+console.log(process.env.SENTRY_AUTH_TOKEN)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
