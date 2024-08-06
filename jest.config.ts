75ecf2243ec902e3da312c96a82285c1173981a6
Dilvika <dilvika@users.noreply.github.com>
2024-08-06 16:40:33 +0700

    refactor: Update Jest configuration and TypeScript compiler options

--- jest.config.ts
+++ jest.config.ts
@@ -2,12 +2,12 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'ts-jest',
     // process `*.tsx` files with `ts-jest`
   },
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
-    '^~(.*)$': '<rootDir>/src$1'
+    '^~(.*)$': '<rootDir>/src$1',
   },
-  setupFiles: ['<rootDir>/src/setupTests.ts']
+  setupFiles: ['<rootDir>/src/setupTests.ts'],
 }
