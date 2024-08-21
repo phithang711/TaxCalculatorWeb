0afc9ab0a790f934b2aee62f06b56b6a9744e724
Dilvika <dilvika@users.noreply.github.com>
2024-08-21 10:26:29 +0700

    refactor: Remove unnecessary console.log statement and unused code in TaxCalculator page

--- src/pages/TaxCalculator/index.tsx
+++ src/pages/TaxCalculator/index.tsx
@@ -24,9 +24,6 @@ const TaxCalculator = () => {
   const [income, setIncome] = useState({})
   const [resultInfo, renewResult] = useTaxCalculator({ income: income })
 
-  const animatedCounterTest = AnimatedCounter({ value: 1000 })
-  console.log(animatedCounterTest)
-
   const toggleColorMode = () => {
     changeTheme()
   }
